4c6041e8c125e684b247c7c302cbe731ea0e0b99
huiter <huiter@vip.qq.com>
2012-11-07 01:03:33 +0800

    更正为璇爷新版

---
+++ application/views/workspace/workspace_canvas_css2.php
@@ -0,0 +1,2 @@
+<link href="http://storage.aliyun.com/pixels/assets/css/buttonstyle.css" rel="stylesheet">
+<link rel="stylesheet" href="http://storage.aliyun.com/pixels/assets/css/jquery.Jcrop.css" type="text/css">
